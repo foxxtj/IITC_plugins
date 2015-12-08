7337b94df444c77486e54ef4af675366bdca0534
Tom Voss <tom@xsthecloud.nl>
2015-12-08 20:40:26 +0100

    Initial commit

---
+++ burster_range_tj.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @id             iitc-plugin-burster-range@tj
+// @name           IITC plugin: Burster range
+// @category       OP Support
+// @version        0.0.1.20151208.2036
+// @namespace      https://github.com/foxxtj/iitc-plugins
+// @updateURL      https://github.com/foxxtj/iitc-plugins/burster_range_tj.js
+// @downloadURL    https://github.com/foxxtj/iitc-plugins/burster_range_tj.js
+// @description    Burster range plugin for IITC
+// @include        https://www.ingress.com/intel*
+// @include        http://www.ingress.com/intel*
+// @match          https://www.ingress.com/intel*
+// @match          http://www.ingress.com/intel*
+// @grant          none
+// ==/UserScript==
